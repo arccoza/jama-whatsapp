ce06acff08465c316374f94761af10dcf1a0e688
Adrien <adrien@arccoza.com>
2021-09-15 12:55:48 +0800

    Add JamaConnector outline

---
+++ jama.go
@@ -0,0 +1,21 @@
+package main
+
+// import (
+// 	firebase
+// )
+
+type JamaConnector struct {
+	conn *firebase
+}
+
+func (c JamaConnector) Publish(msg *Message) {
+
+}
+
+func (c JamaConnector) Subscribe(handler *Handler) {
+
+}
+
+func (c JamaConnector) Query() {
+
+}
