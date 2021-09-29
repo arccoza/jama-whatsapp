8ad32063996fa785823f92d463b1148dbfee4f1d
Adrien <adrien@arccoza.com>
2021-09-29 10:45:15 +0800

    Update qrToURI and qrToTerminal to take string args

--- utils.go
+++ utils.go
@@ -7,13 +7,11 @@ import (
 	"os"
 )
 
-func qrToURI(qrChan chan string) string {
-	tmp := <-qrChan
-	png, _ := qrcode.Encode(tmp, qrcode.Medium, 256)
+func qrToURI(val string) string {
+	png, _ := qrcode.Encode(val, qrcode.Medium, 256)
 	return "data:image/png;base64," + base64.StdEncoding.EncodeToString(png)
 }
 
-func qrToTerminal(qrChan chan string) {
-	val := <-qrChan
+func qrToTerminal(val string) {
 	qrterminal.GenerateHalfBlock(val, qrterminal.L, os.Stdout)
 }
